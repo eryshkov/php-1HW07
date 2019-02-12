1c4f543969f568cf1c3695bd931ee33b6f5c6b6d
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-13 00:56:46 +0300

    1st task is fixed

--- templates/basic.php
+++ templates/basic.php
@@ -1,9 +1,10 @@
-<?php $data = ob_get_contents();
+<?php $serializedData = ob_get_contents();
+$data = unserialize($serializedData, ['allowed_classes' => false]);
 ob_clean();
 ?>
 
-<!--Template goes here-->
-<?php echo $data.'==';?>
+    <!--Template goes here-->
+<?php echo $data . '=='; ?>
 
 <?php $templateText = ob_get_contents();
 ob_end_clean();
