d7608390c51031fa234c329d7a5b3eb9e2ced668
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-13 15:20:28 +0300

    3rd task article is added

---
+++ 03_news/classes/Article.php
@@ -0,0 +1,23 @@
+<?php
+
+class Article
+{
+    protected $fullText;
+    protected $shortText;
+
+    public function __construct(string $shortText, string $fullText)
+    {
+        $this->shortText = $shortText;
+        $this->fullText = $fullText;
+    }
+
+    public function getFullText():string
+    {
+        return $this->fullText;
+    }
+
+    public function getShortText():string
+    {
+        return $this->shortText;
+    }
+}
