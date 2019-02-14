24b025a7ef143ca58c8b758d8b6a5f10ddc13cf9
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-14 12:20:38 +0300

    gallery login page is fixed

--- 02_gallery/login.php
+++ 02_gallery/login.php
@@ -3,8 +3,9 @@ session_start();
 
 require_once __DIR__ . '/functions.php';
 require_once __DIR__ . '/constants.php';
+require_once __DIR__ . '/../classes/View.php';
 
-if (null != getCurrentUser()) {
+if (null !== getCurrentUser()) {
     header('Location: ' . URL_ROOT);
     exit();
 }
@@ -24,30 +25,6 @@ if (checkPassword($userName, $userPassword)) {
     header('Location: ' . URL_ROOT);
     exit();
 }
-?>
 
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-            integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-            crossorigin="anonymous"></script>
-
-    <title>Login</title>
-</head>
-<body>
-<p></p>
-<div class="container">
-    <form action="<?php echo URL_ROOT . 'login.php';?>" method="post">
-        <label>Login: </label><input type="text" name="login"><br>
-        <label>Password: </label><input type="password" name="password"><br>
-        <button type="submit">Login</button>
-    </form>
-</div>
-</body>
-</html>
+$view = new View('galleryLogin.php', URL_ROOT);
+$view->display('galleryLogin.php');
